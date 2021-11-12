dabd3659c80edc0bb545b2c6149a636df7d757d4
Fabrice Daugan <fabrice.daugan@gmail.com>
2021-11-12 18:52:03 +0100

    Add log on actions

--- src/main/java/org/fabdouglas/meeseeks/MeeseeksResource.java
+++ src/main/java/org/fabdouglas/meeseeks/MeeseeksResource.java
@@ -15,10 +15,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Main REST endpoint resource.
+ */
 @RestController
 @RequestMapping("/api")
 public class MeeseeksResource {
+
 	private final AtomicLong counter = new AtomicLong();
+
+	/**
+	 * Shared thread safe counter.
+	 */
 	private static final AtomicLong staticCounter = new AtomicLong();
 
 	@Autowired
@@ -27,42 +35,49 @@ public class MeeseeksResource {
 	@GetMapping()
 	@ResponseBody
 	public String echo(@RequestParam(name = "m", required = false, defaultValue = "hello") String name) {
+		System.out.println("echo message " + name);
 		return name;
 	}
 
 	@GetMapping("echo")
 	@ResponseBody
 	public String echo0(@RequestParam(name = "m", required = false, defaultValue = "hello") String name) {
+		System.out.println("echo message " + name);
 		return name;
 	}
 
 	@GetMapping("counter")
 	@ResponseBody
 	public long counter() {
+		System.out.println("Increment and get counter");
 		return counter.incrementAndGet();
 	}
 
 	@GetMapping("counter-static")
 	@ResponseBody
 	public long counterStatic() {
+		System.out.println("Increment and get static counter");
 		return staticCounter.incrementAndGet();
 	}
 
 	@GetMapping("get")
 	@ResponseBody
 	public long counterNoIncrement() {
+		System.out.println("Get counter");
 		return counter.get();
 	}
 
 	@GetMapping("get-static")
 	@ResponseBody
 	public long counterStaticNoIncrement() {
+		System.out.println("Get static counter");
 		return staticCounter.get();
 	}
 
 	@GetMapping("exit")
 	@ResponseBody
 	public void exit(@RequestParam(name = "code", required = false, defaultValue = "1") int code) {
+		System.out.println("Exit request with code " + code);
 		System.exit(code);
 	}
 
@@ -70,6 +85,7 @@ public class MeeseeksResource {
 	@ResponseBody
 	public void sleep(@RequestParam(name = "millis", required = false, defaultValue = "1000") long millis)
 			throws InterruptedException {
+		System.out.println("Sleep for " + millis + "ms");
 		Thread.sleep(millis);
 	}
 
@@ -77,7 +93,8 @@ public class MeeseeksResource {
 	@ResponseBody
 	public void cpu(@RequestParam(name = "millis", required = false, defaultValue = "1000") long millis)
 			throws InterruptedException {
-		final long start = System.currentTimeMillis();
+		final var start = System.currentTimeMillis();
+		System.out.println("CPU for " + millis + "ms");
 		while (System.currentTimeMillis() - start < millis) {
 			// Nothing to do
 		}
@@ -88,6 +105,7 @@ public class MeeseeksResource {
 	 */
 	@GetMapping("restart")
 	public void restart() {
+		System.out.println("Restart context");
 		final Thread restartThread = new Thread(() -> restartEndpoint.restart(), "Restart");
 		restartThread.setDaemon(false);
 		restartThread.start();
@@ -95,16 +113,18 @@ public class MeeseeksResource {
 
 	@GetMapping("mem")
 	@ResponseBody
-	public void mem(@RequestParam(name = "millis", required = false, defaultValue = "1000") long millis, @RequestParam(name = "nb_kilobytes", required = false, defaultValue = "1000") long nb_kilobytes)
+	public void mem(@RequestParam(name = "millis", required = false, defaultValue = "1000") long millis,
+			@RequestParam(name = "nb_kilobytes", required = false, defaultValue = "1000") long nb_kilobytes)
 			throws InterruptedException {
 		// allocate memory
-		ArrayList<String> mem = new ArrayList<String>();
 		System.out.println("Allocate about " + nb_kilobytes + " Kilobytes of RAM");
-		for(long i=0L; i<nb_kilobytes; i++) {
+		final var mem = new ArrayList<String>();
+		for (var i = 0L; i < nb_kilobytes; i++) {
 			// allocate about 1K of memory
-			for(int j=0; j<20; j++) {
+			for (int j = 0; j < 20; j++) {
 				// 43 bytes + 8 bytes for reference
-				mem.add(new String("123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123"));
+				mem.add(new String(
+						"123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123"));
 			}
 		}
 		// keep the memory for some time
