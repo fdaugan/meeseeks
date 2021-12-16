a79b1043770b29dc683bf7dae161565b35f3a1d5
Fabrice Daugan <fabrice.daugan@gmail.com>
2021-12-16 12:38:18 +0100

    Update Log4j component

--- src/main/java/org/fabdouglas/meeseeks/MeeseeksResource.java
+++ src/main/java/org/fabdouglas/meeseeks/MeeseeksResource.java
@@ -20,11 +20,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Main REST endpoint resource.
  */
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class MeeseeksResource {
 
 	private final AtomicLong counter = new AtomicLong();
@@ -42,21 +45,21 @@ public class MeeseeksResource {
 	@GetMapping()
 	@ResponseBody
 	public String echo(@RequestParam(name = "m", required = false, defaultValue = "hello") String name) {
-		System.out.println("echo message " + name);
+		log.info("echo message " + name);
 		return name;
 	}
 
 	@GetMapping("echo")
 	@ResponseBody
 	public String echo0(@RequestParam(name = "m", required = false, defaultValue = "hello") String name) {
-		System.out.println("echo message " + name);
+		log.info("echo message " + name);
 		return name;
 	}
 
 	@GetMapping("counter")
 	@ResponseBody
 	public long counter() {
-		System.out.println("Increment and get counter");
+		log.info("Increment and get counter");
 		return counter.incrementAndGet();
 	}
 
@@ -69,7 +72,7 @@ public class MeeseeksResource {
 	@ResponseBody
 	public long fileOpen(@RequestParam(name = "files", required = false, defaultValue = "1") int nb)
 			throws FileNotFoundException {
-		System.out.println("Create " + nb + " new file handles");
+		log.info("Create " + nb + " new file handles");
 		for (var files = nb; files-- > 0;) {
 			openedFiles.add(new FileInputStream("/usr/local/app/app.jar"));
 		}
@@ -83,7 +86,7 @@ public class MeeseeksResource {
 	@ResponseBody
 	public long fileClose(@RequestParam(name = "files", required = false, defaultValue = "1") int nb)
 			throws IOException {
-		System.out.println("Create a new file handle");
+		log.info("Create a new file handle");
 		for (var files = nb; files-- > 0;) {
 			openedFiles.poll().close();
 		}
@@ -93,28 +96,28 @@ public class MeeseeksResource {
 	@GetMapping("counter-static")
 	@ResponseBody
 	public long counterStatic() {
-		System.out.println("Increment and get static counter");
+		log.info("Increment and get static counter");
 		return staticCounter.incrementAndGet();
 	}
 
 	@GetMapping("get")
 	@ResponseBody
 	public long counterNoIncrement() {
-		System.out.println("Get counter");
+		log.info("Get counter");
 		return counter.get();
 	}
 
 	@GetMapping("get-static")
 	@ResponseBody
 	public long counterStaticNoIncrement() {
-		System.out.println("Get static counter");
+		log.info("Get static counter");
 		return staticCounter.get();
 	}
 
 	@GetMapping("exit")
 	@ResponseBody
 	public void exit(@RequestParam(name = "code", required = false, defaultValue = "1") int code) {
-		System.out.println("Exit request with code " + code);
+		log.info("Exit request with code " + code);
 		System.exit(code);
 	}
 
@@ -122,7 +125,7 @@ public class MeeseeksResource {
 	@ResponseBody
 	public void sleep(@RequestParam(name = "millis", required = false, defaultValue = "1000") long millis)
 			throws InterruptedException {
-		System.out.println("Sleep for " + millis + "ms");
+		log.info("Sleep for " + millis + "ms");
 		Thread.sleep(millis);
 	}
 
@@ -131,7 +134,7 @@ public class MeeseeksResource {
 	public void cpu(@RequestParam(name = "millis", required = false, defaultValue = "1000") long millis)
 			throws InterruptedException {
 		final var start = System.currentTimeMillis();
-		System.out.println("CPU for " + millis + "ms");
+		log.info("CPU for " + millis + "ms");
 		while (System.currentTimeMillis() - start < millis) {
 			// Nothing to do
 		}
@@ -142,7 +145,7 @@ public class MeeseeksResource {
 	 */
 	@GetMapping("restart")
 	public void restart() {
-		System.out.println("Restart context");
+		log.info("Restart context");
 		final Thread restartThread = new Thread(() -> restartEndpoint.restart(), "Restart");
 		restartThread.setDaemon(false);
 		restartThread.start();
@@ -154,7 +157,7 @@ public class MeeseeksResource {
 			@RequestParam(name = "nb_kilobytes", required = false, defaultValue = "1000") long nb_kilobytes)
 			throws InterruptedException {
 		// allocate memory
-		System.out.println("Allocate about " + nb_kilobytes + " Kilobytes of RAM");
+		log.info("Allocate about " + nb_kilobytes + " Kilobytes of RAM");
 		final var mem = new ArrayList<String>();
 		for (var i = 0L; i < nb_kilobytes; i++) {
 			// allocate about 1K of memory
@@ -165,7 +168,7 @@ public class MeeseeksResource {
 			}
 		}
 		// keep the memory for some time
-		System.out.println("Sleeping for " + millis + " milliseconds");
+		log.info("Sleeping for " + millis + " milliseconds");
 		TimeUnit.MILLISECONDS.sleep(millis);
 	}
 
